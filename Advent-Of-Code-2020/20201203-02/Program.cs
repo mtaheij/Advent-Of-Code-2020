9a095afe27904af6b422bbec67ccbe3ac7e7268d
Michel Taheij <mtaheij@outlook.com>
2020-12-02 18:17:27 +0100

    Added placeholder projects for Day3

---
+++ Advent-Of-Code-2020/20201203-02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20201203_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"..\..\PuzzleInput.txt");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Payload here
+            int numOk = 0;
+
+            sw.Stop();
+            Console.WriteLine(String.Format("There are {0} passwords that are valid", numOk));
+            Console.WriteLine(String.Format("Elapsed time: {0} ms.", sw.ElapsedMilliseconds.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
