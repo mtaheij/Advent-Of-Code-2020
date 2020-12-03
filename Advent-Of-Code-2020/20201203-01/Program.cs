7bbbabea723ab351c10b1250340942f2374cc445
Michel Taheij <mtaheij@outlook.com>
2020-12-03 09:05:14 +0100

    Day 3 solutions

--- Advent-Of-Code-2020/20201203-01/Program.cs
+++ Advent-Of-Code-2020/20201203-01/Program.cs
@@ -13,14 +13,40 @@ namespace _20201203_01
         {
             string[] input = System.IO.File.ReadAllLines(@"..\..\PuzzleInput.txt");
 
+            int slopeX = 3;
+            int slopeY = 1;
+            int patternLength = input[1].Length;
+
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
             // Payload here
-            int numOk = 0;
+            int lineNum = 0;
+            int colNum = 0;
+
+            int numTrees = 0;
+
+            while (lineNum < input.Length)
+            {
+                colNum += slopeX;
+                if (colNum >= patternLength)
+                {
+                    colNum -= patternLength;
+                }
+
+                lineNum += slopeY;
+
+                if (lineNum < input.Length)
+                {
+                    if (input[lineNum].Substring(colNum, 1) == "#" || input[lineNum].Substring(colNum, 1) == "X")
+                    {
+                        numTrees++;
+                    }
+                }
+            }
 
             sw.Stop();
-            Console.WriteLine(String.Format("There are {0} passwords that are valid", numOk));
+            Console.WriteLine(String.Format("There are {0} trees encountered", numTrees));
             Console.WriteLine(String.Format("Elapsed time: {0} ms.", sw.ElapsedMilliseconds.ToString()));
             Console.ReadLine();
         }
--- Advent-Of-Code-2020/20201203-02/Program.cs
+++ Advent-Of-Code-2020/20201203-02/Program.cs
@@ -17,12 +17,55 @@ namespace _20201203_02
             sw.Start();
 
             // Payload here
-            int numOk = 0;
+            int numTrees1 = CountTrees(input, 1, 1);
+            Console.WriteLine(String.Format("There are {0} trees encountered for slope = 1,1", numTrees1));
+
+            int numTrees2 = CountTrees(input, 3, 1);
+            Console.WriteLine(String.Format("There are {0} trees encountered for slope = 3,1", numTrees2));
+
+            int numTrees3 = CountTrees(input, 5, 1);
+            Console.WriteLine(String.Format("There are {0} trees encountered for slope = 5,1", numTrees3));
+
+            int numTrees4 = CountTrees(input, 7, 1);
+            Console.WriteLine(String.Format("There are {0} trees encountered for slope = 7,1", numTrees4));
+
+            int numTrees5 = CountTrees(input, 1, 2);
+            Console.WriteLine(String.Format("There are {0} trees encountered for slope = 1,2", numTrees5));
 
             sw.Stop();
-            Console.WriteLine(String.Format("There are {0} passwords that are valid", numOk));
+            Console.WriteLine(String.Format("The answer = {0}", numTrees1 * numTrees2 * numTrees3 * numTrees4 * numTrees5));
             Console.WriteLine(String.Format("Elapsed time: {0} ms.", sw.ElapsedMilliseconds.ToString()));
             Console.ReadLine();
+        }
+
+        static int CountTrees(string[] input, int slopeX, int slopeY)
+        {
+            int patternLength = input[0].Length;
+            int lineNum = 0;
+            int colNum = 0;
+
+            int numTrees = 0;
+
+            while (lineNum < input.Length)
+            {
+                colNum += slopeX;
+                if (colNum >= patternLength)
+                {
+                    colNum -= patternLength;
+                }
+
+                lineNum += slopeY;
+
+                if (lineNum < input.Length)
+                {
+                    if (input[lineNum].Substring(colNum, 1) == "#" || input[lineNum].Substring(colNum, 1) == "X")
+                    {
+                        numTrees++;
+                    }
+                }
+            }
+
+            return numTrees;
         }
     }
 }
