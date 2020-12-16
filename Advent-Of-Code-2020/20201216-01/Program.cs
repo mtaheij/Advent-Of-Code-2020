ed3f0d589e76e64f5336f5292f5896c813657495
Michel Taheij <mtaheij@outlook.com>
2020-12-16 21:05:08 +0100

    Day16 solutions

---
+++ Advent-Of-Code-2020/20201216-01/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _20201216_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines(@"PuzzleInput.txt");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Payload here
+            Dictionary<string, List<int>> validnumbers = new Dictionary<string, List<int>>();
+            List<int> validAll = new List<int>();
+
+            int r = 0;
+            do
+            {
+                string inp = input[r];
+                if (!string.IsNullOrWhiteSpace(inp))
+                {
+                    string[] parts = inp.Split(':');
+                    string sKey = parts[0];
+
+                    string[] pairs = parts[1].Split(' ');
+                    string[] firstPair = pairs[1].Split('-');
+                    string[] secondPair = pairs[3].Split('-');
+
+                    int minFirst = int.Parse(firstPair[0]);
+                    int maxFirst = int.Parse(firstPair[1]);
+                    int minSecond = int.Parse(secondPair[0]);
+                    int maxSecond = int.Parse(secondPair[1]);
+
+                    List<int> numbers = new List<int>();
+
+                    for (int a = minFirst; a <= maxFirst; a++)
+                    {
+                        numbers.Add(a);
+                        validAll.Add(a);
+                    }
+                    for (int a = minSecond; a <= maxSecond; a++)
+                    {
+                        numbers.Add(a);
+                        validAll.Add(a);
+                    }
+
+                    validnumbers.Add(sKey, numbers);
+                }
+
+                r++;
+            } while (!input[r].Equals("your ticket:"));
+
+            int tser = 0;
+
+            for (int i = r+4; i < input.Length; i++)
+            {
+                string[] values = input[i].Split(',');
+
+                foreach (string s in values)
+                {
+                    int val = int.Parse(s);
+                    if (validAll.Contains(val))
+                    {
+
+                    }
+                    else
+                    {
+                        tser += val;
+                    }
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine(String.Format("The answer is: {0}.", tser));
+            Console.WriteLine(String.Format("Elapsed time: {0} ms.", sw.ElapsedMilliseconds.ToString()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Advent-Of-Code-2020/20201216-02/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+using MoreLinq;
+using static AdventOfCode.Helpers;
+
+namespace _20201216_02
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			byte[] input = System.IO.File.ReadAllBytes(@"PuzzleInput.txt");
+
+			if (input == null) return;
+
+			var segments = input.GetLines(StringSplitOptions.None)
+				.Segment(s => string.IsNullOrWhiteSpace(s))
+				.ToArray();
+
+			var rulesDescriptions = segments[0];
+			var myTicket = segments[1].Last().Split(',').Select(int.Parse).ToArray();
+			var otherTickets = segments[2].Skip(2).Select(l => l.Split(',').Select(int.Parse).ToArray()).ToArray();
+
+			var rules = rulesDescriptions
+				.Select(l => (
+					name: l[..l.IndexOf(':')],
+					values: Regex.Matches(l, @"(\d+)-(\d+)").OfType<Match>()
+						.Select(m => (lo: int.Parse(m.Groups[1].Value), hi: int.Parse(m.Groups[2].Value)))
+						.ToArray()))
+				.ToArray();
+
+			var validNumbers = rules.SelectMany(x => x.values).ToArray();
+			var PartA = otherTickets
+				.SelectMany(t => t)
+				.Where(n => !validNumbers.Any(x => n.Between(x.lo, x.hi)))
+				.Sum()
+				.ToString();
+
+			var validTickets = otherTickets
+				.Where(t => t.All(n => validNumbers.Any(x => n.Between(x.lo, x.hi))))
+				.ToArray();
+
+			var candidates = rules
+				.Select(r => (
+					r.name,
+					indexes: Enumerable.Range(0, validTickets[0].Length)
+						.Where(i => validTickets.All(t => r.values.Any(v => t[i].Between(v.lo, v.hi))))
+						.ToList()))
+				.ToList();
+
+			var map = new List<(string name, int index)>();
+			while (map.Count < rules.Length)
+			{
+				var nextCandidate = candidates.First(c => c.indexes.Count == 1);
+				var index = nextCandidate.indexes[0];
+				map.Add((nextCandidate.name, index));
+				candidates.RemoveAll(c => c.name == nextCandidate.name);
+				foreach (var (_, indexes) in candidates)
+					indexes.Remove(index);
+			}
+
+			var PartB = map
+				.Where(r => r.name.StartsWith("departure"))
+				.Select(r => myTicket[r.index])
+				.Aggregate(1L, (l, r) => l * r)
+				.ToString();
+		}
+	}
+}
